3ee534dd3c41b04c6c967b798f19e23399afe574
meshack2213 <meshackzkl@gmail.com>
2025-02-19 15:20:18 +0300

    header

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -6,7 +6,8 @@ import React from "react";
 const Header = () => {
   return (
     <div>
-      <div>
+      <div className="w-11/12 max-w-3xl text-center mx-auto h-screen
+      flex flex-col items-center justify-center gap-4">
         <Image src={assets.profile_img} alt="" className="rounded-full w-32" />
       </div>
       <h3
